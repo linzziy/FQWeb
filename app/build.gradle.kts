7b02ffe65c922bf6ccf58946fc77cf68fd859349
fengyuecanzhu <1021300691@qq.com>
2023-07-31 14:37:04 +0800

    Bump to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.fycz.fqweb"
         minSdk = 24
         targetSdk = 33
-        versionCode = 143
-        versionName = "1.4.3"
+        versionCode = 144
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
